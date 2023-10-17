84260d0f0f20c41ed4fd909b4be1f1e533dd7a08
Katerina Kuznetsova <izorog@gmail.com>
2023-10-18 01:01:49 +0300

    Полная вертка
    Дописаны остальные элементы

--- banner/wp-content/themes/Banner/index.php
+++ banner/wp-content/themes/Banner/index.php
@@ -18,18 +18,58 @@
             /* align-items: center; */
             position: absolute;
         }
+        .DownLine
+        {
+            margin-top: 133px;
+            width: 775px;
+            position: absolute;
+            padding-left: 50px;
+        }
+        .DownLine_row-1
+        {
+            height: 114px;
+        }
+        .DownLine_col-1-3
+        {
+            margin-top: 30px;
+        }
+        .DownLine_col-2
+        {
+            font-weight:900;
+            color: #D23B60;
+            font-size: 32px;
+            margin-top: 37px;
+        }
+        .DownLine_col-4
+        {
+            font-size: 96px;
+            font-family: Mplus 1p bold;
+            font-weight:600;
+            margin-top: 3px;
+            color: #D23B60;
+            margin-right: -4px;
+        }
+
+        .DownLine_col-6
+        {
+            font-size: 26px;
+            font-family: Gilroy;
+            font-weight:500;
+            color: #999999;
+            text-transform: uppercase;
+        }
         .Img {
             position: absolute;
         }
         .ImgPosition
         {
             top: 0;
-        right: 1; /* Располагает изображение справа */
-        z-index: 1;
-        margin-left: 391px;
-        margin-right: 25px;
-        margin-top: 7px;
-        margin-bottom: 93px;
+            right: 1; /* Располагает изображение справа */
+            z-index: 1;
+            margin-left: 391px;
+            margin-right: 25px;
+            margin-top: 7px;
+            margin-bottom: 93px;
         }
         .left-up-text
         {
@@ -39,14 +79,15 @@
         line-height: 42px;
         word-wrap: break-word;
         font-family: 'Gilroy', sans-serif;
+        letter-spacing: 0px;
         }
         .right-up-text
         {
         color: white;
         font-size: 26px;
-        font-weight: 800;
+        font-weight: 700;
         line-height: 36px;
-        letter-spacing: 0.90px;
+        letter-spacing: 0px;
         word-wrap: break-word;
         font-family: 'Gilroy', sans-serif;
         }
@@ -72,7 +113,7 @@
     <img class="Img" src="<?php echo get_template_directory_uri(); ?>\png\Узи1.png" width="775" height="300">
     <div class="upLine">
         <div class=" row align-items-center no-gutters" style="padding-top:15px">
-            <div  class="col text-center">
+            <div  class="custom-col text-center" style="width: 150px; margin-left:30px ">
                 <p class="left-up-text">УЗИ</p> 
             </div>
             <div class="col text-center">
@@ -85,21 +126,34 @@
             </div>
         </div>
     </div>
-    <!-- <div class="" style="top:0;">
-        <div class=" row align-items-center no-gutters" style="padding-top:15px">
-            <div  class="col text-center">
-                <p class="left-up-text">УЗИ</p> 
+
+    <div class="container DownLine">
+        <div class="row flex-row DownLine_row-1">
+            <div class="custom-col max-width-50 DownLine_col-1-3" style="width: 60px;">
+                <img class="Img" src="<?php echo get_template_directory_uri(); ?>\png\Pattern1.png" width="57" height="44">
             </div>
-            <div class="col text-center">
-                <div class="img-up-container">
-                <img class="Img" src="<?php echo get_template_directory_uri(); ?>\png\+.png" width="24" height="24">
-                </div>
+            <div class="custom-col min-width-100 DownLine_col-2" style="width: 215px;">
+                <div class="content-box text-center">за 1800 ₽</div>
+            </div>
+            <div class="custom-col min-width-100 DownLine_col-1-3" style="width: 129px;">
+                <img class="Img" src="<?php echo get_template_directory_uri(); ?>\png\Group 3.png" width="109" height="44">
+            </div>
+            <div class="custom-col min-width-100 DownLine_col-4" style="width: 100px;">
+                <div class="content-box">АКЦИЯ</div>
+            </div>
+
+        </div>
+        <div class="row flex-row">
+            <div class="custom-col" style="width: 30px;">
+                <img class="Img" src="<?php echo get_template_directory_uri(); ?>\png\Star.png" width="36" height="36">
             </div>
             <div class="col">
-            <p class="right-up-text">Консультация <br> Гинеколога</p> 
+                <div class="content-box DownLine_col-6">Врач высшей категории!</div>
             </div>
         </div>
-    </div> -->
+    </div>
+
+
 </div>
 
 <img class="Img ImgPosition" src="<?php echo get_template_directory_uri(); ?>\png\Узи2.png" width="359" height="200">
